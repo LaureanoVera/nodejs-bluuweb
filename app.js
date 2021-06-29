b0b36464adf6e77805ff5259cdc9a5803f71e73a
LaureanoVera <laureanoigvera@gmail.com>
2021-06-29 15:40:54 -0300

    package.json y npm

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const { fruits, prices } = require("./fruits");
 
+const cowsay = require('cowsay');
+console.log(cowsay.say({
+  text: 'Hello lalalala',
+  e: '-0',
+  T: 'U'
+}));
+
 fruits.forEach((item) => {
   console.log(`Fruit: ${item}`);
 });
 
-console.log('Prices: ${prices}');
+console.log(`Prices: ${prices}`);
