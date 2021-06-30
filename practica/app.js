d66441b4e5cf0c71c8204de083bde7cb66bf4190
LaureanoVera <laureanoigvera@gmail.com>
2021-06-30 13:24:46 -0300

    services y team

--- practica/app.js
+++ practica/app.js
@@ -7,19 +7,19 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 // ===== STATIC =====
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
 // ===== ROUTES =====
 app.get("/", (req, res) => {
-  res.render('index')
+  res.render("index");
 });
 
 app.get("/services", (req, res) => {
-  res.end("Services");
+  res.render("services");
 });
 
 app.get("/team", (req, res) => {
-  res.end("Team");
+  res.render('team', {team: ['Lauri', 'Ivan', 'Gerardo']})
 });
 
 app.listen(port, () => {
