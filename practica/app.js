2cc5062dbfdac79a90acc3820f0a0257edbf8271
LaureanoVera <laureanoigvera@gmail.com>
2021-06-30 09:47:50 -0300

    views y public

--- practica/app.js
+++ practica/app.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const port = 4008;
 
+// ===== ENGINE =====
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// ===== STATIC =====
+app.use(express.static(__dirname + '/public'))
+
+// ===== ROUTES =====
 app.get("/", (req, res) => {
-  res.end("Home");
+  res.render('index')
 });
 
 app.get("/services", (req, res) => {
   res.end("Services");
 });
 
-app.get('/team', (req, res) => {
-  res.end('Team')
-})
+app.get("/team", (req, res) => {
+  res.end("Team");
+});
 
 app.listen(port, () => {
   console.log(`Server Listening on port ${port}`);
