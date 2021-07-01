5421559046bac302a4d5691f884fadbaacc62fc9
LaureanoVera <laureanoigvera@gmail.com>
2021-07-01 14:02:53 -0300

    mongodb

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("mascotas", { 
-    title: 'Mascotas',
-    arrayMascotas: [
-      { id: "afwa52h", name: "rex", description: "rex descripcion" },
-      { id: "6w26ya8", name: "dum", description: "dum descripcion" },
-    ],
-  });
+const Mascota = require("../models/mascota");
+
+router.get("/", async (req, res) => {
+  try {
+    const arrayMascotasDB = await Mascota.find();
+    console.log(arrayMascotasDB);
+
+    res.render("mascotas", {
+      title: "Mascotas",
+      arrayMascotas: arrayMascotasDB
+      // arrayMascotas: [
+      //   { id: "afwa52h", name: "rex", description: "rex descripcion" },
+      //   { id: "6w26ya8", name: "dum", description: "dum descripcion" },
+      // ],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
